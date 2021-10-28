4c96c410d2e5c6e1703b7f35d04f723d2b69132a
Rituraj <rituraj@cronj.com>
2021-10-28 13:32:09 +0530

    New commit

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 const user = require('./models/users')
 const bodyParser = require('body-parser')
 const jsonParser = bodyParser.json()
-mongoose.connect('mongodb+srv://avenger:NWSXWIBX73UHVUoj@cluster0.xja0h.mongodb.net/tutorial?retryWrites=true&w=majority',
+mongoose.connect('mongodb+srv://avenger:Ritu2198@cluster0.xja0h.mongodb.net/tutorial?retryWrites=true&w=majority',
 {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -28,4 +28,34 @@ user.find({name: regex}).then((result)=>{
 })
 })
 
+//sorting in asc
+app.get('/users/name/asc', (req,res)=>{
+    user.find({}).sort({
+        name: 1
+    }).then((data) => {
+         res.json(data)
+    })
+})
+app.get('/users/name/dsc', (req,res)=>{
+    user.find({}).sort({
+        name: -1
+    }).then((data) => {
+         res.json(data)
+    })
+})
+
+app.get('/users/phone/asc', (req,res)=>{
+    user.find({}).sort({
+        phone: 1
+    }).then((data) => {
+         res.json(data)
+    })
+})
+app.get('/users/phone/dsc', (req,res)=>{
+    user.find({}).sort({
+        phone: -1
+    }).then((data) => {
+         res.json(data)
+    })
+})
 app.listen(4000)
